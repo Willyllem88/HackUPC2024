c273490e810185082c25dbdc1f401a4edc4d6d30
Guillem Cabré Farré <82611244+Willyllem88@users.noreply.github.com>
2024-05-07 08:33:15 +0200

    Quitar funciones que no se usan de dataCollection.ino

--- dataCollection.ino
+++ dataCollection.ino
@@ -51,16 +51,3 @@ void loop() {
   timer = timer + 1;
   delay(1000);
 }
-
-//It returns the voltage read by the pin
-float readAdc(int pin) {
-  int tempSensorValue = analogRead(A0);
-  return float(tempSensorValue)*(5./4095.);
-}
-
-void printTemp(float Vtemp) {
-    Serial.print(-88.23*Vtemp+86.17);
-    Serial.print(" ºC  -  ");
-    Serial.print(Vtemp);
-    Serial.println(" V");
-}
